5a57cfef4a8fe115863c1d8f849a32358d9a43a1
setdotpy <ch317503@gmail.com>
2021-07-28 15:46:12 +0900

    coco_eval

---
+++ tensorrtx/yolov5/coco_eval.py
@@ -0,0 +1,12 @@
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+# Run COCO mAP evaluation
+# Reference: https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocoEvalDemo.ipynb
+cocoGt = COCO('../../saving_nomalize.json')
+cocoDt = cocoGt.loadRes('./result_ann.json')
+imgIds = sorted(cocoGt.getImgIds())
+cocoEval = COCOeval(cocoGt, cocoDt, 'bbox')
+cocoEval.params.imgIds = imgIds
+cocoEval.evaluate()
+cocoEval.accumulate()
+cocoEval.summarize()
